256890f0f3a6e1fece621b812c0a66e619c67802
dennis gega <dennis.gega@uconn.edu>
2025-12-31 13:52:01 -0500

    single command with no arguments

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main() {
 	while (1) {
@@ -12,11 +13,21 @@ int main() {
 		ssize_t linelen;
 
 		linelen = getline(&line, &linecap, stdin);
+		line[strcspn(line, "\n")] = '\0';
 
-		if (strcmp(line, "exit\n") == 0)
+		if (strcmp(line, "exit") == 0)
 			exit(0);
-		
-		execv(line, NULL);	
+
+		int rc = fork();
+		if (rc < 0) {
+			fprintf(stderr, "fork failed\n");
+			exit(1);
+		} else if (rc == 0) {
+			char *args[] = { line, NULL };
+			execv(args[0], args);
+		} else {
+			int rc_wait = wait(NULL);
+		}
 	}
 
 	return 0;
