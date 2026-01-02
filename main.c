c8aa1ba874d9d3594b3be73f519d5085f7868ac5
dennis gega <dennis.gega@uconn.edu>
2026-01-02 18:08:38 -0500

    built in commands

--- main.c
+++ main.c
@@ -4,10 +4,12 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+char error_message[30] = "An error has occurred\n";
+char path[50] = "/bin/";
+
 int main() {
 	while (1) {
 		printf("dennish> ");
-
 
 		// get raw line from stdin
 		char *line = NULL;
@@ -15,11 +17,10 @@ int main() {
 		ssize_t linelen;
 		linelen = getline(&line, &linecap, stdin);
 
-
 		// parse input into array
 		char *argv[10], **ap = argv;
-		char buf[50] = "/bin/";
-		argv[0] = buf;
+		argv[0] = path;
+
 		for (int i = 1; i < 10; ++i) argv[i] = NULL;
 		int argc = 0;
 		while (1) {
@@ -27,8 +28,16 @@ int main() {
 			if (token == NULL || *token == '\n' || *token == '\0')
 				break;
 
-			// append command name to $PATH
-			if (argc == 0) strcat(argv[0], token);
+			// append command name to $PATH if command is in there
+			if (argc == 0) {
+				char buf[50]; strcpy(buf, path);
+				strcat(buf, token);
+
+				if (access(buf, F_OK) == 0)
+					strcat(argv[0], token);
+				else *ap = token;
+			}
+
 			else *ap = token;
 			++argc;
 
@@ -36,8 +45,36 @@ int main() {
 				++ap;
 		}
 
-		if (strcmp(argv[0], "exit") == 0)
+		// builtin commands
+		if (strcmp(argv[0], "exit") == 0) {
+			if (argc != 1) {
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
+			}
+
 			exit(0);
+		}
+
+		if (strcmp(argv[0], "cd") == 0) {
+			if (argc != 2) { 
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
+			}
+
+			if (chdir(argv[1]) == -1) {
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
+			}
+		}
+
+		if (strcmp(argv[0], "path") == 0) {
+			if (argc != 2) {
+				write(STDERR_FILENO, error_message, strlen(error_message));
+				exit(1);
+			}
+
+			strcpy(path, argv[1]);
+		}
 
 
 		int rc = fork();
