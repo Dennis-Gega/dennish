619c475f781cf70386e4f919c1632435fd7a6052
dennis gega <dennis.gega@uconn.edu>
2026-01-01 21:41:22 -0500

    basic command loop up and running

--- main.c
+++ main.c
@@ -8,23 +8,41 @@ int main() {
 	while (1) {
 		printf("dennish> ");
 
+
+		// get raw line from stdin
 		char *line = NULL;
 		size_t linecap = 0;
 		ssize_t linelen;
-
 		linelen = getline(&line, &linecap, stdin);
-		line[strcspn(line, "\n")] = '\0';
 
-		if (strcmp(line, "exit") == 0)
+
+		// parse input into array
+		char *argv[10], **ap = argv;
+		argv[0] = "/bin/";
+		for (int i = 1; i < 10; ++i) argv[i] = NULL;
+		int argc = 0;
+		while (1) {
+			char* token = strsep(&line, " \n");
+			if (token == NULL || *token == '\n' || *token == '\0')
+				break;
+
+			*ap = token;
+			++argc;
+
+			if (ap != &argv[10])
+				++ap;
+		}
+
+		if (strcmp(argv[0], "exit") == 0)
 			exit(0);
+
 
 		int rc = fork();
 		if (rc < 0) {
 			fprintf(stderr, "fork failed\n");
 			exit(1);
 		} else if (rc == 0) {
-			char *args[] = { line, NULL };
-			execv(args[0], args);
+			execv(argv[0], argv);
 		} else {
 			int rc_wait = wait(NULL);
 		}
